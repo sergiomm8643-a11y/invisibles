163f8c711bed5fc0851155dfd0c8773733c6b5dd
sergiomm8643-a11y <sergiomm8643@gmail.com>
2026-06-09 10:33:45 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,5 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-ZPJ104WBBG");
+ReactGA.send("pageview");
 createRoot(document.getElementById("root")).render(<App />);
