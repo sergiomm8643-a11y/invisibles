3b9286a6690d9a40eebaaf96427943f8f886229e
sergiomm8643-a11y <sergiomm8643@gmail.com>
2026-06-22 16:12:11 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -17,17 +17,17 @@ const collections = [
   {
     name: "Negro",
     images: [
-      "/camiseta-especial-front.jpg.PNG.2.png",
-      "/camiseta-especial-back.jpg.PNG.2.png",
       "/camiseta-especial-hanger.jpg.PNG.2.png",
+      "/camiseta-especial-back.jpg.PNG.2.png",
+      "/camiseta-especial-front.jpg.PNG.2.png",
     ],
   },
   {
     name: "Blanco",
     images: [
-      "/camiseta-especial-front.jpg.PNG.blanco.png",
-      "/camiseta-especial-back.jpg.PNG.blanco.png",
       "/camiseta-especial-hanger.jpg.PNG.blanco.png",
+      "/camiseta-especial-back.jpg.PNG.blanco.png",
+      "/camiseta-especial-front.jpg.PNG.blanco.png",
     ],
   },
 ],
